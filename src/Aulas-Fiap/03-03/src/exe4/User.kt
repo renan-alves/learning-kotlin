3739eecabcd56e39bb69688c5171334e4c938559
Renan Augusto Alves da Costa <30834451+renan-alves@users.noreply.github.com>
2020-03-03 20:10:27 -0300

    Update User.kt

--- src/Aulas-Fiap/03-03/src/exe4/User.kt
+++ src/Aulas-Fiap/03-03/src/exe4/User.kt
@@ -4,3 +4,4 @@ data class User(val i: Int, val s: String)
 //    : Comparable<User> {
 //    override fun compareTo(other: User): Int = this.s.compareTo(other.s)
 //    override fun toString(): String = "Id: $i Nome: $s"
+//    }
